93fab9c71ed8143905c80e316b192e4b1a82b0e2
Shubham Kumar <shubham9905374230@gmail.com>
2023-06-30 22:13:55 +0530

    tracking API

---
+++ src/API.js
@@ -0,0 +1,11 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7e90f547e7msh254f69d1d4adffdp1a9b9djsned0f7033974c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const url2 = "https://api.openweathermap.org/data/2.5";
+export const key ="d2d56fb378fd08106afe7e9743509001"  ////have o move to gitIgnore
